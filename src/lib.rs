c345cb238a65234e761602459c3fe54e8025d062
Brad Bell <bradbell@seanet.com>
2025-06-07 07:12:52 -0700

    main: lib.rs: remove code that was moved to add_op.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -88,66 +88,6 @@ impl From<Float> for AD {
     }
 }
 //
-// eval_add_vv_fn
-fn eval_add_vv_fn(
-    vec: &mut Vec<Float>, _con: &Vec<Float>, arg: &[Index], res: Index) {
-    assert_eq!( arg.len(), 2);
-    vec[ res ] = vec[ arg[0] ] + vec[ arg[1] ];
-}
-//
-// eval_add_vc_fn
-fn eval_add_vc_fn(
-    vec: &mut Vec<Float>, con: &Vec<Float>, arg: &[Index], res: Index) {
-    assert_eq!( arg.len(), 2);
-    vec[ res ] = vec[ arg[0] ] + con[ arg[1] ];
-}
-//
-// std::ops::ADD for AD
-fn record_add(tape : &mut TapeInfo, lhs : &AD, rhs : &AD) -> (Index, Index) {
-    let mut new_tape_id   = 0;
-    let mut new_var_index = 0;
-    if tape.recording {
-        let var_lhs    = lhs.tape_id == tape.tape_id;
-        let var_rhs    = rhs.tape_id == tape.tape_id;
-        if var_lhs || var_rhs {
-            new_tape_id = tape.tape_id;
-            if var_lhs && var_rhs {
-                new_var_index = tape.n_var;
-                tape.n_var   += 1;
-                tape.op_vec.push(ADD_VV_OP);
-                tape.op2arg.push( tape.arg_vec.len() );
-                tape.arg_vec.push( lhs.var_index );
-                tape.arg_vec.push( rhs.var_index );
-            } else if var_lhs {
-                if rhs.value == 0.0 {
-                    new_var_index = lhs.var_index;
-                } else {
-                    new_var_index = tape.n_var;
-                    tape.n_var   += 1;
-                    tape.op_vec.push(ADD_VC_OP);
-                    tape.op2arg.push( tape.arg_vec.len() );
-                    tape.arg_vec.push( lhs.var_index );
-                    tape.arg_vec.push( tape.con_vec.len() );
-                    tape.con_vec.push( rhs.value );
-                }
-            } else {
-                if lhs.value == 0.0 {
-                    new_var_index = rhs.var_index;
-                } else {
-                    new_var_index = tape.n_var;
-                    tape.n_var   += 1;
-                    tape.op_vec.push(ADD_VC_OP);
-                    tape.op2arg.push( tape.arg_vec.len() );
-                    tape.arg_vec.push( rhs.var_index );
-                    tape.arg_vec.push( tape.con_vec.len() );
-                    tape.con_vec.push( lhs.value );
-                }
-            }
-        }
-    }
-    (new_tape_id, new_var_index)
-}
-//
 // independent
 pub fn independent( x : &[Float] ) {
     THERADS_RECORDER.with_borrow_mut( |tape| {
