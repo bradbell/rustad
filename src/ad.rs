9ea3543f982a00858b0e6e40f2fb984112219093
Brad Bell <bradbell@seanet.com>
2025-06-10 03:21:29 -0700

    main: Add some AD documentation.

--- src/ad.rs
+++ src/ad.rs
@@ -6,6 +6,18 @@
 use crate::Index;
 use crate::Float;
 //
+/// The AD type acts like the Float type.
+/// It has the addition capability to recoerd functions and
+/// store it in an [ADFun](crate::ADFun) object.
+///
+/// # variable
+/// An AD object is a variable if the following two conditions hold:
+/// 1. [This threads recorder](crate::THIS_THREAD_RECORDER)
+///    is currently recording
+/// 2. The threads recorder and the AD object have the same *tape_id*
+///
+/// # constant
+/// If an AD object is not a variable it is referred to as a constant.
 #[derive(Copy, Clone)]
 pub struct AD {
     pub tape_id   : Index,
--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@
 /// # Example
 /// ```
 /// let date = *rustad::YEAR_MONTH_DAY;
-/// assert_eq!(date, "2025.6.9");
+/// assert_eq!(date, "2025.6.10");
 /// ```
 pub const YEAR_MONTH_DAY: std::sync::LazyLock<&str> =
-   std::sync::LazyLock::new( || "2025.6.9" );
+   std::sync::LazyLock::new( || "2025.6.10" );
 // ----------------------------------------------------------------------------
 //
 // utility
