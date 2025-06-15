3773151e1a2740708c30ae5344f7224bc38003e8
Brad Bell <bradbell@seanet.com>
2025-06-15 06:09:03 -0700

    main: tests amd bench mark: change px, py -> rx, ry.

--- src/bin/bench_op.rs
+++ src/bin/bench_op.rs
@@ -8,7 +8,7 @@ use rustad::function;
 fn test_add_vv() {
     let x  : Vec<Float> = vec![ 1.0, 2.0, 3.0 ];
     let dx : Vec<Float> = vec![ 4.0, 5.0, 6.0 ];
-    let py : Vec<Float> = vec![ 7.0, 8.0 ];
+    let ry : Vec<Float> = vec![ 7.0, 8.0 ];
     let ax      = function::ad_domain(&x);
     let ay_0    = ax[0] + ax[1];
     let ay_1    = ax[1] + ax[2];
@@ -17,7 +17,7 @@ fn test_add_vv() {
     let trace   = false;
     let (y, v0) = f.forward_zero(&x, trace);
     let dy      = f.forward_one(&dx, &v0, trace);
-    let px      = f.reverse_one(&py, &v0, trace);
+    let rx      = f.reverse_one(&ry, &v0, trace);
     //
     assert_eq!( y[0], x[0] + x[1] );
     assert_eq!( y[1], x[1] + x[2] );
@@ -25,15 +25,15 @@ fn test_add_vv() {
     assert_eq!( dy[0], dx[0] + dx[1] );
     assert_eq!( dy[1], dx[1] + dx[2] );
     //
-    assert_eq!( px[0], py[0] );
-    assert_eq!( px[1], py[0] + py[1] );
-    assert_eq!( px[2], py[1] );
+    assert_eq!( rx[0], ry[0] );
+    assert_eq!( rx[1], ry[0] + ry[1] );
+    assert_eq!( rx[2], ry[1] );
 }
 
 fn test_mul_vv() {
     let x  : Vec<Float> = vec![ 1.0, 2.0, 3.0 ];
     let dx : Vec<Float> = vec![ 4.0, 5.0, 6.0 ];
-    let py : Vec<Float> = vec![ 6.0, 8.0 ];
+    let ry : Vec<Float> = vec![ 6.0, 8.0 ];
     let ax      = function::ad_domain(&x);
     let ay_0    = ax[0] * ax[1];
     let ay_1    = ax[1] * ax[2];
@@ -42,7 +42,7 @@ fn test_mul_vv() {
     let trace   = false;
     let (y, v0) = f.forward_zero(&x, trace);
     let dy      = f.forward_one(&dx, &v0, trace);
-    let px      = f.reverse_one(&py, &v0, trace);
+    let rx      = f.reverse_one(&ry, &v0, trace);
     //
     assert_eq!( y[0], x[0] * x[1] );
     assert_eq!( y[1], x[1] * x[2] );
@@ -50,9 +50,9 @@ fn test_mul_vv() {
     assert_eq!( dy[0], dx[0] * x[1] + x[0] * dx[1] );
     assert_eq!( dy[1], dx[1] * x[2] + x[1] * dx[2] );
     //
-    assert_eq!( px[0], py[0] * x[1] );
-    assert_eq!( px[1], py[0] * x[0] + py[1] * x[2] );
-    assert_eq!( px[2], py[1] * x[1] );
+    assert_eq!( rx[0], ry[0] * x[1] );
+    assert_eq!( rx[1], ry[0] * x[0] + ry[1] * x[2] );
+    assert_eq!( rx[2], ry[1] * x[1] );
 }
 
 fn bench( name : &String, test_case : fn() ) {
