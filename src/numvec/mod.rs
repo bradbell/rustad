4bf7d104c63eeeaa422c6c6c9bdeb2027d512015
Brad Bell <bradbell@seanet.com>
2025-09-01 04:03:05 -0700

    main: numvec/mod.rs: more setup_this_value_type to setup module.

--- src/numvec/mod.rs
+++ src/numvec/mod.rs
@@ -63,33 +63,38 @@ pub use op::info::{
     OpInfo
 };
 // ---------------------------------------------------------------------------
-/// Set up rustad to do calculations with value type V; see
-/// [doc_generic_v](crate::numvec::ad::doc_generic_v) .
-///
-/// This macro must be executed once for any type *V*  where `AD<V>` is used.
-/// The rustad package automatically executes this macro
-/// for the following types: `f32` , `f64` , `NumVec<f32>`, `NumVec<f64>`.
-///
-/// This macro can be invoked from anywhere given the following use statements:
-/// ```text
-///     use std::sync::LazyLock;
-///     use std::thread::LocalKey;
-///     use std::cell::RefCell;
-///     use crate::numvec::ad::AD;
-/// ```
-///
-use std::sync::LazyLock;
-use std::thread::LocalKey;
-use std::cell::RefCell;
-macro_rules! setup_this_value_type{ ($V:ty) => {
-        crate::numvec::tape::impl_this_thread_tape!($V);
-        crate::numvec::op::info::impl_global_op_info_vec!($V);
-        crate::numvec::ad::impl_value_op_ad!($V);
-} }
-setup_this_value_type!(f32);
-setup_this_value_type!(f64);
-setup_this_value_type!( NumVec<f32> );
-setup_this_value_type!( NumVec<f64> );
+pub mod setup {
+    use std::sync::LazyLock;
+    use std::thread::LocalKey;
+    use std::cell::RefCell;
+    use crate::numvec::ad::AD;
+    use crate::numvec::NumVec;
+    ///
+    /// Set up rustad to do calculations with value type V; see
+    /// [doc_generic_v](crate::numvec::ad::doc_generic_v) .
+    ///
+    /// This macro must be executed once for any type *V*  where `AD<V>` is used.
+    /// The rustad package automatically executes this macro
+    /// for the following types: `f32` , `f64` , `NumVec<f32>`, `NumVec<f64>`.
+    ///
+    /// This macro requires the following use statements:
+    /// ```text
+    ///     use std::sync::LazyLock;
+    ///     use std::thread::LocalKey;
+    ///     use std::cell::RefCell;
+    ///     use crate::numvec::ad::AD;
+    /// ```
+    ///
+    macro_rules! setup_this_value_type{ ($V:ty) => {
+            crate::numvec::tape::impl_this_thread_tape!($V);
+            crate::numvec::op::info::impl_global_op_info_vec!($V);
+            crate::numvec::ad::impl_value_op_ad!($V);
+    } }
+    setup_this_value_type!(f32);
+    setup_this_value_type!(f64);
+    setup_this_value_type!( NumVec<f32> );
+    setup_this_value_type!( NumVec<f64> );
+}
 // ---------------------------------------------------------------------------
 //
 // NumVec
