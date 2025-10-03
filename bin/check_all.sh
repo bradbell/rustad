f724ab310c332089a71f2a1b91e4bc3d5bc2ba86
Brad Bell <bradbell@seanet.com>
2025-10-03 04:33:38 -0700

    ad_atom: check_all.sh: clean out old target/$build_type/deps

--- bin/check_all.sh
+++ bin/check_all.sh
@@ -22,6 +22,17 @@ then
    rm rustad.long-type-*
 fi
 #
+# target/deps/debug
+# target/deps/release
+for build_type in debug release
+do
+   dir="target/$build_type/deps"
+   if [ -e $dir ]
+   then
+      rm $dir/*
+   fi
+done
+#
 # typos
 if ! which typos > /dev/null
 then
