d2cbcc598c9aa3f444fa3af35df7b3b5b6886a95
Brad Bell <bradbell@seanet.com>
2025-10-04 02:17:52 -0700

    ad_atom: atom: forward_zero_ad: use atom for this evaluation.

--- examples/atom/forward_zero.rs
+++ examples/atom/forward_zero.rs
@@ -5,11 +5,14 @@
 use rustad::{
     AD,
     IndexT,
-    ad_from_value,
+    call_atom,
 };
 //
-// V
-use super::V;
+// V, ATOM_ID_VEC
+use super::{
+    V,
+    ATOM_ID_VEC
+};
 //
 // sumsq_forward_zero_value
 pub fn sumsq_forward_zero_value(
@@ -24,7 +27,7 @@ pub fn sumsq_forward_zero_value(
     let mut sumsq_zero = 0 as V;
     for j in 0 .. domain_zero.len() {
         sumsq_zero += &( domain_zero[j] * domain_zero[j] );
-        var_zero.push( *domain_zero[j] )
+        var_zero.push( (*domain_zero[j]).clone() );
     }
     if trace {
         println!("Begin Trace: sumsq_forward_zero_value");
@@ -43,26 +46,38 @@ pub fn sumsq_forward_zero_value(
 pub fn sumsq_forward_zero_ad(
     var_zero     : &mut Vec< AD<V> > ,
     domain_zero  : Vec<& AD<V> >     ,
-    _call_info   : IndexT            ,
+    call_info    : IndexT            ,
     trace        : bool              ,
 ) -> Vec< AD<V> >
 {   //
-    // var_zero, sumsq_zero
+    // atom_id
+    let atom_id = ATOM_ID_VEC.with_borrow( |atom_id_vec| 
+        atom_id_vec[call_info as usize]
+    );
+    //
+    // n_domain
+    let n_domain = domain_zero.len();
+    //
+    // var_zero, domain_zero_clone
     assert_eq!( var_zero.len(), 0 );
-    let mut sumsq_zero = ad_from_value( 0 as V );
-    for j in 0 .. domain_zero.len() {
-        sumsq_zero += &( domain_zero[j] * domain_zero[j] );
-        var_zero.push( (*domain_zero[j]).clone() )
+    let mut domain_zero_clone : Vec< AD<V> > = Vec::with_capacity( n_domain);
+    for j in 0 .. n_domain {  
+        domain_zero_clone.push( (*domain_zero[j]).clone() );
+        var_zero.push( (*domain_zero[j]).clone() );
     }
+    //
+    // sumsq_zero
+    let sumsq_zero = call_atom(domain_zero_clone, atom_id, call_info, trace);
+    //
     if trace {
         println!("Begin Trace: sumsq_forward_zero_value");
         print!("domain_zero = [ ");
-        for j in 0 .. domain_zero.len() {
-                print!("{}, ", domain_zero[j]);
+        for j in 0 .. n_domain {
+                print!("{}, ", var_zero[j]);
         }
         println!("]");
-        println!("sumsq_zero = {}", sumsq_zero);
+        println!("sumsq_zero = {:?}", sumsq_zero);
         println!("End Trace: sumsq_forward_zero_value");
     }
-    vec![ sumsq_zero ]
+    sumsq_zero
 }
