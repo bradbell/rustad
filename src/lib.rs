0ebfc133212f2d8c2f439bf311b386dea4cdd7c3
Brad Bell <bradbell@seanet.com>
2025-10-06 02:02:34 -0700

    main: remove src/checkpoint.rs (dead code).

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,7 @@ where
 /// # Example
 /// ```
 /// let date = *rustad::YEAR_MONTH_DAY;
-/// assert_eq!(date, "2025.10.5");
+/// assert_eq!(date, "2025.10.6");
 /// ```
 pub const YEAR_MONTH_DAY: std::sync::LazyLock<&str> =
-   std::sync::LazyLock::new( || "2025.10.5" );
+   std::sync::LazyLock::new( || "2025.10.6" );
