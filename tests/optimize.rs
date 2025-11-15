175ec17d29190b0acf5e11f8337248255d3fa406
Brad Bell <bradbell@seanet.com>
2025-11-14 17:10:15 -0700

    main: tests: optimize.rs: convert value type from float to AzFloat.

--- tests/optimize.rs
+++ tests/optimize.rs
@@ -3,6 +3,7 @@
 // SPDX-FileContributor: 2025 Bradley M. Bell
 //
 use rustad::{
+    AzFloat,
     ad_from_value,
     start_recording,
     stop_recording,
@@ -10,15 +11,15 @@ use rustad::{
 //
 // test_left_zero_one_both_ad
 fn test_left_zero_one_both_ad() {
-    type V      = f64;
+    type V      = AzFloat<f64>;
     let trace   = false;
     //
-    let x  : Vec<V>  = vec![ 3.0 ];
+    let x  : Vec<V>  = vec![ V::from(3.0) ];
     //
     let ax  = start_recording( x.clone() );
-    let a1  = &ad_from_value(0.0 as V) + &ax[0]; // optimized to ax[0]
-    let a2  = &ad_from_value(1.0 as V) * &a1;    // optimized to ax[0]
-    let a3  = &ad_from_value(0.0 as V) * &a2;    // constant 0
+    let a1  = &ad_from_value(V::from(0.0)) + &ax[0]; // optimized to ax[0]
+    let a2  = &ad_from_value(V::from(1.0)) * &a1;    // optimized to ax[0]
+    let a3  = &ad_from_value(V::from(0.0)) * &a2;    // constant 0
     let a4  = &a3 + &a2;                         // optimized to ax[0]
     let ay  = vec![ a4 ];
     let f   = stop_recording(ay);
@@ -34,17 +35,17 @@ fn test_left_zero_one_both_ad() {
 //
 // test_left_zero_one_right_ad
 fn test_left_zero_one_right_ad() {
-    type V      = f64;
+    type V      = AzFloat<f64>;
     let trace   = false;
     //
-    let x  : Vec<V>  = vec![ 3.0 ];
+    let x  : Vec<V>  = vec![ V::from(3.0) ];
     //
     let ax  = start_recording( x.clone() );
-    let a1  = &(0.0 as V) + &ax[0]; // optimized to ax[0]
-    let a2  = &(1.0 as V) * &a1;    // optimized to ax[0]
-    let a3  = &(0.0 as V) * &a2;    // constant 0
+    let a1  = &(V::from(0.0)) + &ax[0]; // optimized to ax[0]
+    let a2  = &(V::from(1.0)) * &a1;    // optimized to ax[0]
+    let a3  = &(V::from(0.0)) * &a2;    // constant 0
     let a4  = &a3 + &a2;            // optimized to ax[0]
-    let a5  = &(0.0 as V) / &a4;    // constant 0
+    let a5  = &(V::from(0.0)) / &a4;    // constant 0
     let a6  = &a5 + &a4;            // optimized to ax[0]
     let ay  = vec![ a6 ];
     let f   = stop_recording(ay);
@@ -60,17 +61,17 @@ fn test_left_zero_one_right_ad() {
 //
 // test_right_zero_one_both_ad
 fn test_right_zero_one_both_ad() {
-    type V      = f64;
+    type V      = AzFloat<f64>;
     let trace   = false;
     //
-    let x  : Vec<V>  = vec![ 3.0 ];
+    let x  : Vec<V>  = vec![ V::from(3.0) ];
     //
     let ax  = start_recording( x.clone() );
-    let a1  = &ax[0] + &ad_from_value(0.0 as V); // optimized to ax[0]
-    let a2  = &a1    * &ad_from_value(1.0 as V); // optimized to ax[0]
-    let a3  = &a2    * &ad_from_value(0.0 as V); // constant 0
+    let a1  = &ax[0] + &ad_from_value(V::from(0.0)); // optimized to ax[0]
+    let a2  = &a1    * &ad_from_value(V::from(1.0)); // optimized to ax[0]
+    let a3  = &a2    * &ad_from_value(V::from(0.0)); // constant 0
     let a4  = &a2 + &a3;                         // optimized to ax[0]
-    let a5  = &a4    / &ad_from_value(1.0 as V); // optimized to ax[0]
+    let a5  = &a4    / &ad_from_value(V::from(1.0)); // optimized to ax[0]
     let ay  = vec![ a5 ];
     let f   = stop_recording(ay);
     //
@@ -85,17 +86,17 @@ fn test_right_zero_one_both_ad() {
 //
 // test_right_zero_one_left_ad
 fn test_right_zero_one_left_ad() {
-    type V      = f64;
+    type V      = AzFloat<f64>;
     let trace   = false;
     //
-    let x  : Vec<V>  = vec![ 3.0 ];
+    let x  : Vec<V>  = vec![ V::from(3.0) ];
     //
     let ax  = start_recording( x.clone() );
-    let a1  = &ax[0] + &(0.0 as V); // optimized to ax[0]
-    let a2  = &a1    * &(1.0 as V); // optimized to ax[0]
-    let a3  = &a2    * &(0.0 as V); // constant 0
+    let a1  = &ax[0] + &(V::from(0.0)); // optimized to ax[0]
+    let a2  = &a1    * &(V::from(1.0)); // optimized to ax[0]
+    let a3  = &a2    * &(V::from(0.0)); // constant 0
     let a4  = &a2 + &a3;            // optimized to ax[0]
-    let a5  = &a4    / &(1.0 as V); // optimized to ax[0]
+    let a5  = &a4    / &(V::from(1.0)); // optimized to ax[0]
     let ay  = vec![ a5 ];
     let f   = stop_recording(ay);
     //
