be740d2a7d3fa4a0f7b22825466e71ae4da5fb26
Brad Bell <bradbell@seanet.com>
2026-02-23 05:47:11 -0700

    main: developer doc for unary and binary operators.

--- src/op/binary/mod.rs
+++ src/op/binary/mod.rs
@@ -5,6 +5,19 @@
 //! This module defines the objects that evaluate binary operators.
 //!
 //! Link to [parent module](super)
+//!
+//! # Arithmetic Operators :
+//! ADD_XX_OP, SUB_XX_OP, MUL_XX_OP, DIV_XX_OP
+//! where XX is PP, PV, or VP.
+//!
+//! # Compare Operators :
+//! LT_OP, LE_OP, EQ_OP, NE_OP, GE_OP, GT_OP
+//!
+//! # Operator Arguments
+//! | Index | Meaning |
+//! | ----- | ------- |
+//! | 0     | Variable, dynamic, or constant index for left hand side  |
+//! | 1     | Variable, dynamic, or constant index for right hand side |
 // ---------------------------------------------------------------------------
 // sub-modules
 pub mod common;
--- src/op/unary/mod.rs
+++ src/op/unary/mod.rs
@@ -5,17 +5,39 @@
 //! This module defines the objects that evaluate unary operators.
 //!
 //! Link to [parent module](super)
+//!
+//! # Operators :
+//! [comment]: <> (BEGIN_SORT_THIS_LINE_PLUS_1)
+//! ABS_OP,
+//! COSH_OP,
+//! COS_OP,
+//! EXP_OP,
+//! MINUS_OP,
+//! SIGNUM_OP,
+//! SINH_OP,
+//! SIN_OP,
+//! SQRT_OP,
+//! TANH_OP,
+//! TAN_OP,
+//! [comment]: <> (END_SORT_THIS_LINE_MINUS_1)
+//!
+//! # Operator Arguments
+//! | Index | Meaning |
+//! | ----- | ------- |
+//! | 0     | Variable, dynamic, or constant index for operator argument |
 // ---------------------------------------------------------------------------
 // sub-modules
-pub mod common;
-pub mod sqrt;
-pub mod tanh;
-pub mod tan;
-pub mod sinh;
-pub mod cosh;
+// BEGIN_SORT_THIS_LINE_PLUS_1
 pub mod abs;
-pub mod signum;
-pub mod exp;
-pub mod sin;
+pub mod common;
 pub mod cos;
+pub mod cosh;
+pub mod exp;
 pub mod minus;
+pub mod signum;
+pub mod sin;
+pub mod sinh;
+pub mod sqrt;
+pub mod tan;
+pub mod tanh;
+// END_SORT_THIS_LINE_MINUS_1
