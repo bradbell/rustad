d63f26ce5fb360596484d425d6ec67d32ee4c745
Brad Bell <bradbell@seanet.com>
2026-03-06 07:30:05 -0700

    main: Advance date.

--- src/lib.rs
+++ src/lib.rs
@@ -156,9 +156,9 @@ where
 /// # Example
 /// ```
 /// let date = rustad::YEAR_MONTH_DAY;
-/// assert_eq!(date, "2026.3.5");
+/// assert_eq!(date, "2026.3.6");
 /// ```
-pub const YEAR_MONTH_DAY : &str = "2026.3.5";
+pub const YEAR_MONTH_DAY : &str = "2026.3.6";
 //
 // TRAITS_RS
 /// is the source code for the [core](crate::float::traits) module.
